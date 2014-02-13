888a992f91b45fad68c038896d12007c1f5acd2b
williamw <welense@gmail.com>
2014-02-13 13:26:54 -0600

    Initial commit

---
+++ play.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+# encoding: utf-8
+ 
+import requests
+import xml.dom.minidom
+
+sonos_ip = "10.0.1.3:1400"
+soap_action = '"urn:schemas-upnp-org:service:AVTransport:1#Play"'
+request = u"""<s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/" s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/"><s:Body><u:Play xmlns:u="urn:schemas-upnp-org:service:AVTransport:1"><InstanceID>0</InstanceID><Speed>1</Speed></u:Play></s:Body></s:Envelope>"""
+
+encoded_request = request.encode('utf-8')
+url = "http://" + sonos_ip + "/MediaRenderer/AVTransport/Control"
+ 
+headers = {"HOST": sonos_ip,
+           "CONTENT-TYPE": "text/xml; charset=UTF-8",
+           "CONTENT-LENGTH": len(encoded_request),
+           "USER-AGENT": """Linux UPnP/1.0 Sonos/24.0-68210o (MDCR_MacBookAir4,2)""",
+           "SOAPACTION": soap_action
+			}
+                           
+response = requests.post(url=url,
+                         headers = headers,
+                         data = encoded_request,
+                         verify=False)
+
+response = xml.dom.minidom.parseString(response.text)
+response = response.toprettyxml()
+
+print unicode(response)
